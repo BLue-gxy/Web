88be3a3382c804f31f455e666b01b0003709c6da
BLue-gxy <13017559250@163.com>
2024-02-26 18:02:33 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/github-actions-deploy-dynamic',
+  base: '/dist',
   // server: {
   //   proxy: {
   //     '/api': {
